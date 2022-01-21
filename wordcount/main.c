160f15601b7a874b3e7a5fe56b59679afbb8e0aa
masinde70 <5812481+masinde70@users.noreply.github.com>
2022-01-21 14:00:17 +0200

    word count

--- wordcount/main.c
+++ wordcount/main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include "word_count.h"
+#include <string.h>
+
 
 int main() {
     printf("Hello, World!\n");
     return 0;
+}
+
+int word_count(const char *input_text, word_count_word_t *words){
+   char word_list[MAX_WORDS_LENGTH];
 }
