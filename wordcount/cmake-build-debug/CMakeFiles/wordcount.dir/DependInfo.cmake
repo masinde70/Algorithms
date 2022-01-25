cf7da874972fcddf2b59dc592982a0db200885ab
masinde70 <5812481+masinde70@users.noreply.github.com>
2022-01-25 18:34:02 +0200

    algot

--- wordcount/cmake-build-debug/CMakeFiles/wordcount.dir/DependInfo.cmake
+++ wordcount/cmake-build-debug/CMakeFiles/wordcount.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/Users/masinde/CLionProjects/Algorithms/wordcount/main.c" "CMakeFiles/wordcount.dir/main.c.o" "gcc" "CMakeFiles/wordcount.dir/main.c.o.d"
+  "/home/masinde/Algorithms/Algorithms/wordcount/main.c" "CMakeFiles/wordcount.dir/main.c.o" "gcc" "CMakeFiles/wordcount.dir/main.c.o.d"
   )
 
 # Targets to which this target links.
